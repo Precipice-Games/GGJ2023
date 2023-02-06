d33a542fea5c805d17c3e166759514be77800418
ThePhard <47338095+PaulHarden@users.noreply.github.com>
2023-02-05 23:50:10 -0500

    UI / VFX
    - UI repolish.
    - Removed "Main" from build index.
    - Created wind FX for each of the 4 types of prefabs and attached.
    - Shortened script a little and attached Dissolve shader to tendril prefab, doesnt work perfectly like a sphere or cube though.

--- UntitledRootsGame/Assets/Roots/MaterialLibrary/Dissolver.cs
+++ UntitledRootsGame/Assets/Roots/MaterialLibrary/Dissolver.cs
@@ -5,29 +5,19 @@ namespace Roots.MaterialLibrary
 	public class Dissolver : MonoBehaviour
 	{
 		private Material _mat;
-		private bool _hide;
 		public float dissolveSpeed;
-		public string activateKey;
 
 		private void Start()
 		{
-			_mat = GetComponent<Renderer>().material;
+			_mat = GetComponent<SkinnedMeshRenderer>().material;
 		}
 
-		private void Update()
+		public void Update()
 		{
-			if(Input.GetKeyDown(activateKey))
-			{
-				_hide = !_hide;
-			}
-
-			if(_hide)
+			if(Input.GetKeyDown(KeyCode.Tab))
 			{
 				_mat.SetFloat("_NoiseStrength", Mathf.MoveTowards(_mat.GetFloat("_NoiseStrength"), 1f, dissolveSpeed * Time.deltaTime));
-			}
-			else
-			{
-				_mat.SetFloat("_NoiseStrength", Mathf.MoveTowards(_mat.GetFloat("_NoiseStrength"), -1f, dissolveSpeed * Time.deltaTime));
+				Destroy(this.gameObject);
 			}
 		}
 	}
